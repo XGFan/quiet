8b126804d95557203d2d92f32424eabc3d76af94
Guofan Xu <xuguofan@live.com>
2020-01-17 15:13:12 +0800

    我也忘记改了什么了……

--- src/main/kotlin/com/xulog/quiet/PostService.kt
+++ src/main/kotlin/com/xulog/quiet/PostService.kt
@@ -2,11 +2,12 @@ package com.xulog.quiet
 
 import com.vladsch.flexmark.ext.footnotes.FootnoteExtension
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
+import com.vladsch.flexmark.ext.gitlab.GitLabExtension
 import com.vladsch.flexmark.ext.tables.TablesExtension
 import com.vladsch.flexmark.ext.toc.TocExtension
 import com.vladsch.flexmark.html.HtmlRenderer
 import com.vladsch.flexmark.parser.Parser
-import com.vladsch.flexmark.util.options.MutableDataSet
+import com.vladsch.flexmark.util.data.MutableDataSet
 import com.xulog.quiet.Constants.MARKDOWN_EXTS
 import org.slf4j.LoggerFactory
 import java.nio.file.Files
@@ -21,15 +22,14 @@ import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 
-class PostService
-constructor(private val quiet: Path, private val quietConfig: QuietConfig) {
+class PostService(private val quiet: Path, private val quietConfig: QuietConfig) {
     companion object {
         val DateParser: DateTimeFormatter = DateTimeFormatterBuilder()
-                .append(DateTimeFormatter.ofPattern("[yyyy-MM-dd HH:mm:ss][yyyy-MM-dd HH:mm][yyyy-MM-dd HH][yyyy-MM-dd]"))
-                .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-                .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-                .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-                .toFormatter()
+            .append(DateTimeFormatter.ofPattern("[yyyy-MM-dd HH:mm:ss][yyyy-MM-dd HH:mm][yyyy-MM-dd HH][yyyy-MM-dd]"))
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+            .toFormatter()
     }
 
     private val logger = LoggerFactory.getLogger("[${quiet.fileName}] Worker")
@@ -41,16 +41,21 @@ constructor(private val quiet: Path, private val quietConfig: QuietConfig) {
     private val parser: Parser
     private val renderer: HtmlRenderer
 
-
     //todo 略微尴尬的一些操作
     init {
         //创建markdown parse和renderer
         val options = MutableDataSet()
-                .set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(),
-                        FootnoteExtension.create(),
-                        TocExtension.create(),
-                        StrikethroughExtension.create()))
-                .set(HtmlRenderer.SOFT_BREAK, "<br/>")
+            .set(
+                Parser.EXTENSIONS,
+                listOf(
+                    TablesExtension.create(),
+                    FootnoteExtension.create(),
+                    TocExtension.create(),
+                    StrikethroughExtension.create(),
+                    GitLabExtension.create()
+                )
+            )
+            .set(HtmlRenderer.SOFT_BREAK, "<br/>")
         parser = Parser.builder(options).build()
         renderer = HtmlRenderer.builder(options).build()
 
@@ -87,22 +92,23 @@ constructor(private val quiet: Path, private val quietConfig: QuietConfig) {
     }
 
     fun countPage(pageSize: Int): Int = posts.asSequence().filterNot { it.draft }
-            .filter { it.categories.intersect(quietConfig.hiddenDirs).isEmpty() }.count()
+        .filter { it.categories.intersect(quietConfig.getHiddenDirs()).isEmpty() }.count()
 
     fun findPost(offset: Int, limit: Int): Page<Post> {
-        val filter = posts.asSequence().filterNot { it.draft }
-                .filter { it.categories.intersect(quietConfig.hiddenDirs).isEmpty() }
-        return filter.drop(offset).take(limit) to filter.count()
+        val total = posts.asSequence().filterNot { it.draft }
+            .filter { it.categories.intersect(quietConfig.getHiddenDirs()).isEmpty() }
+        return total.drop(offset).take(limit) to total.count()
     }
 
     fun findPost(uri: String): Post? = posts.asSequence().filterNot { it.draft }.find { it.matchUri(uri) }
 
-    fun findPost(cats: List<String>): List<Post> = posts.asSequence().filterNot { it.draft }.filter { it.matchCat(cats) }.toList()
+    fun findPost(cats: List<String>): List<Post> =
+        posts.asSequence().filterNot { it.draft }.filter { it.matchCat(cats) }.toList()
 
     fun findChildren(cats: List<String>): List<Category> = posts.asSequence().filter { it.isGrandPa(cats) }
-            .map { it.categories.take(cats.size + 1) }
-            .distinct()
-            .map { it.last() to it.joinToString("/", prefix = "/category/") }.toList()
+        .map { it.categories.take(cats.size + 1) }
+        .distinct()
+        .map { it.last() to it.joinToString("/", prefix = "/category/") }.toList()
 
     /**
      * 解析markdown并预渲染文件
@@ -133,21 +139,26 @@ constructor(private val quiet: Path, private val quietConfig: QuietConfig) {
         val creationTime = LocalDateTime.ofInstant(attr.creationTime().toInstant(), ZoneId.systemDefault())
         val lastModifiedTime = LocalDateTime.ofInstant(attr.lastModifiedTime().toInstant(), ZoneId.systemDefault())
         val (create, update) = getDate(creationTime, lastModifiedTime, map)
-        return Post(markdown,
-                filename,
-                map["title"]?.cleanQuotes() ?: filename,
-                map["slug"]?.cleanQuotes(),
-                markdown.categoriesOf(root),
-                create,
-                update,
-                map["draft"]?.toBoolean() ?: false,
-                content
+        return Post(
+            markdown,
+            filename,
+            map["title"]?.cleanQuotes() ?: filename,
+            map["slug"]?.cleanQuotes(),
+            markdown.categoriesOf(root),
+            create,
+            update,
+            map["draft"]?.toBoolean() ?: false,
+            content
         )
     }
 
     private fun String.toLocalDateTime(): LocalDateTime = LocalDateTime.parse(this, DateParser)
 
-    private fun getDate(fileCreation: LocalDateTime, fileModify: LocalDateTime, header: Map<String, String>): Pair<LocalDateTime, LocalDateTime> {
+    private fun getDate(
+        fileCreation: LocalDateTime,
+        fileModify: LocalDateTime,
+        header: Map<String, String>
+    ): Pair<LocalDateTime, LocalDateTime> {
         var create = header["create"]?.toLocalDateTime() ?: min(fileCreation, fileModify)
         var update = header["update"]?.toLocalDateTime() ?: max(fileCreation, fileModify)
         create = min(create, update)
@@ -156,15 +167,18 @@ constructor(private val quiet: Path, private val quietConfig: QuietConfig) {
     }
 }
 
-data class Post(val path: Path,
-                val name: String, //文件名,不带后缀
-                val title: String,
-                val slug: String? = null, //url？
-                val categories: List<String> = emptyList(),//目录
-                val create: LocalDateTime, //创建时间
-                val update: LocalDateTime,//最后修改时间
-                var draft: Boolean = false,
-                var content: String) {
+@Suppress("MemberVisibilityCanBePrivate")
+data class Post(
+    val path: Path,
+    val name: String, //文件名,不带后缀
+    val title: String,
+    val slug: String? = null, //url？
+    val categories: List<String> = emptyList(),//目录
+    val create: LocalDateTime, //创建时间
+    val update: LocalDateTime,//最后修改时间
+    var draft: Boolean = false,
+    var content: String
+) {
     val dateUri = "/${create.format(Constants.DATE_IN_URL)}/${slug ?: title}.html"
 
     val categoryUri = "${categories.joinToString("/", prefix = "/", postfix = "/")}${slug ?: title}.html"
